e141c9b8df2b57e2981b99ea90df00fbf7009b64
DiegoSofttek <diegoa.cruz@softtek.com>
2026-09-10 14:42:38 -0600

    refactor: simplify error handling in getUserById function

--- src/repositories/user_repo.ts
+++ src/repositories/user_repo.ts
@@ -1,12 +1,12 @@
 import { DatabaseError } from "../utils/errors";
 
 export async function getUserById(id: string) {
-  try {
-    if (id === "crash") {
-      throw new DatabaseError("SELECT * FROM users", "Connection lost to Postgres");
+    try {
+        if (id === "crash") {
+        throw new DatabaseError("SELECT * FROM users", "Connection lost to Postgres");
+        }
+        return { id, name: "Test User" };
+    } catch (error) {
+        throw new Error("DB Failed");
     }
-    return { id, name: "Test User" };
-  } catch (error) {
-    throw new Error("DB Failed");
-  }
 }
